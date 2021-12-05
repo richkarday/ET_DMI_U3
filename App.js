fc22c29156738156cfb92ea996e2b710fc5eafc8
Ricardo <ricardo030800@hotmail.com>
2021-12-05 14:28:21 -0600

    Se agregó la navegacion por tabs y la pantalla de libro funcional

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import Client from './pages/Client';
+import BookScreen from './pages/Book';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeColor: 'blue',
+        inactiveColor: 'white',
+        style: {
+          backgroundColor: 'rgba(48, 51, 118, 0.8)',
+          position: 'absolute',
+          borderTopWidth: 0,
+        },
+      }}>
+      <Tab.Screen
+        name="Client"
+        component={Client}
+        options={{
+          title: 'Client'
+        }}
+      />
+      <Tab.Screen
+        name="Book"
+        component={BookScreen}
+        options={{
+          title: 'Book'
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
 
   return (
-    <Client/>
+    <NavigationContainer> 
+      <BottomTabNavigator/>
+    </NavigationContainer>
   );
 }
 
